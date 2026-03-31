3530f567c37ace25522bcd46cae92d993b66dfb3
lucas fraga <152925470+doidaolucao@users.noreply.github.com>
2026-03-31 18:39:26 -0300

    Implementando calculadora IMC
    Após o hello world no início do código, foi feita a implementação da calculadora IMC como desafio iniciante de operador aritmético e lógico.

--- hello-world/Main.java
+++ hello-world/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
+// Boas vindas ao meu início de aprendizado em Java!
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Olá mundo!");
+        System.out.println("Olá mundo!");
+        calculadora(args);
+    }
+
+    // Agora que já tiramos a maldição, vamos ao primeiro exercício!! :)
+    // Calculadora IMC, utilizado para avaliar o peso ideal com base entre peso e altura.
+
+    static void calculadora(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira o seu peso:");
+        double peso = input.nextDouble();
+
+        System.out.println("Agora, insira a sua altura:");
+        double altura = input.nextDouble();
+
+        System.out.println("Calculando o seu IMC...");
+        double imc = peso / (altura * altura);
+
+        System.out.printf("Seu IMC é: %.2f\n", imc);
+
+        if (imc < 18) {
+            System.out.printf("Seu peso está abaixo do normal.");
+        } else if (imc > 18 && imc < 25) {
+            System.out.printf("Seu peso está normal.");
+        } else {
+            System.out.printf("Você está com excesso de peso.");
+        }
+
+        System.out.print("");
+        input.close();
     }
 }
